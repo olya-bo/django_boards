fdfff54a22652c8fc8f3507155a5a021999128e6
Olha <mycleverowl@gmail.com>
2021-09-12 23:11:33 +0300

    tests added

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,8 @@ from django.urls import path
 from boards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('boards/<int:pk>', views.board_topics, name='board_topics'),
+    path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
+    path('admin/', admin.site.urls),
 ]
