c690fa1421f4c5f7d62212361dfdebc3282f1fa2
Olha <mycleverowl@gmail.com>
2021-09-12 22:49:33 +0300

    part two completed

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from boards import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('boards/<int:pk>', views.board_topics, name='board_topics'),
 ]
