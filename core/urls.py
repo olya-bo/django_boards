79007e6614d78759b7fb0c281fdb9e478590a517
Olha <o.bocharova@m.sharksw.com>
2021-09-14 11:19:14 +0300

    added tests, created new url, view, template

--- core/urls.py
+++ core/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
 
     path('boards/<int:pk>', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
 
     path('admin/', admin.site.urls),
 ]
